2cb82344232a18b3cd66657d262db6e3434a3905
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-06-28 21:24:43 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setimage] = useState([]);
+  console.log("images", image);
+
+  const handlesubmit = (files) => {
+    setimage(files);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        onChange={(event) => {
+          handlesubmit(event.target.files);
+        }}
+      />
     </div>
   );
 }
